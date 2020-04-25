a1de81734470a74fdf46f8e16c95804e0472bebf
Luba <luponom@mail.ru>
2020-04-25 22:01:37 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,106 @@
+
+
+import "./style.css";
+import "./js/Card.js";
+import "./js/CardList.js";
+import "./js/API.js";
+import "./js/FormValidator.js";
+import "./js/Popup.js";
+import "./js/UserInfo.js";
+import {Api} from './js/API.js';
+import {Card} from './js/Card.js';
+import {CardList} from './js/CardList.js';
+import {FormValidator} from './js/FormValidator.js';
+import {Popup} from './js/Popup.js';
+import {UserInfo} from './js/UserInfo';
+export {  editForm, popupEditButton, editButton, closeEditForm, formEdit, errorStatus, nameValueForm, jobValueForm};
+
+export const listContainer = document.querySelector('.places-list');
+
+const newForm = document.forms.new;
+const editForm = document.querySelector('.popup__form-edit');
+const popupEditButton = document.querySelector('.popup__edit-button'); 
+
+const editButton = document.querySelector('.user-info__edit-button');
+const closeEditForm = document.querySelector('#popup__close-edit');
+const formEdit = document.forms.edit;
+let errorStatus = false;
+
+let nameValueForm = '';
+let jobValueForm = '';  
+
+
+ const api = new Api({
+  headers: {
+    authorization: '6af9b0d5-fde9-4d91-8997-d01e8790c6cd',
+    'Content-Type': 'application/json'
+  }
+});
+
+const card = new Card();
+const cardList = new CardList(card, api);
+const validator = new FormValidator();
+const popup = new Popup();
+const userInfo = new UserInfo(
+  document.querySelector(".user-info__name"),
+  document.querySelector(".user-info__job"),
+  document.querySelector('.user-info__photo'),
+  formEdit.elements.nameUser,
+  formEdit.elements.jobUser,
+  nameValueForm,
+  jobValueForm,
+  api
+);
+
+//userCard
+function inputUserCard(event) {
+  const name = event.currentTarget.elements.name;
+  const img = event.currentTarget.elements.link;
+
+  if (name.value.length === 0 || img.value.length === 0) {
+    document.querySelector('.popup__card-button').setAttribute('disabled', '');
+
+  }
+  else {
+    document.querySelector('.popup__card-button').removeAttribute('disabled');
+  }
+
+
+}
+function addCardUser(event) {
+
+  event.preventDefault();
+  const imageUrl = newForm.elements.link.value;
+  const cardName = newForm.elements.name.value;
+  if (imageUrl.length !== 0 && cardName.length !== 0) {
+
+    cardList.addCard(imageUrl, cardName);
+
+    popup.toggleNewPopup();
+  }
+  return [imageUrl, cardName];
+
+}
+newForm.addEventListener('input', inputUserCard);
+newForm.addEventListener('submit', addCardUser);
+
+cardList.getApi();
+cardList.addButtons();
+userInfo.infoApi();
+userInfo.infoEditListener();
+
+
+editForm.addEventListener('input', validator.setEventListeners);
+
+formEdit.addEventListener("submit", function (event) {
+  event.preventDefault();
+  userInfo.setUserInfoEdit();
+  userInfo.inputInfoApi();
+  userInfo.setUserInfoEdit();
+
+
+});
+
+ 
+
+
---
+++ src/js/UserInfo.js
@@ -0,0 +1,77 @@
+
+export class UserInfo {
+
+  constructor(
+    userName, //container
+    userJob,
+    userPhoto,//container
+    inputName,
+    inputJob,
+    nameValueForm,//textCont
+    jobValueForm,//textCont
+    api
+  ) {
+    this.userName = userName;
+    this.userJob = userJob;
+    this.userPhoto = userPhoto;
+    this.inputName = inputName;
+    this.inputJob = inputJob;
+    this.api = api;
+    this.nameValueForm = nameValueForm;
+    this.jobValueForm = jobValueForm;
+    this.inputInfoApi = this.inputInfoApi.bind(this);
+  }
+  string(nameStr, jobStr) {
+
+
+    this.inputName.value = nameStr;
+    this.inputJob.value = jobStr;
+  }
+  setUserInfoEdit() {
+    this.nameValueForm = this.inputName.value;
+    this.jobValueForm = this.inputJob.value;
+
+  }
+  updateUserInfo (data) {
+    this.userName.textContent = data.name;
+    this.userJob.textContent = data.about;
+    this.userPhoto.setAttribute('style',
+      `background-image: url(${data.avatar})`);
+  }
+  infoEditListener() {
+    document.querySelector('.user-info__edit-button').addEventListener('click', function () {
+      userInfo.string(document.querySelector(".user-info__name").textContent, document.querySelector(".user-info__job").textContent);
+    });
+
+  };
+  infoApi() {
+    this.api.getUserInfo()
+      .then((data) =>  {
+
+        return this.updateUserInfo(data);
+
+
+
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+  }
+  inputInfoApi() {
+    this.api.setUserInfo(this.nameValueForm, this.jobValueForm)
+      .then(function(res)  {
+        popup.toggleEditPopup();
+        return this.updateUserInfo(res);
+
+
+
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  } /**/
+}
+
+
+
