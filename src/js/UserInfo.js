0511fe7e27f998d0c4f5ec70234bb6f6555b930b
Luba <luponom@mail.ru>
2020-04-26 21:04:44 +0300

    no

--- src/js/UserInfo.js
+++ src/js/UserInfo.js
@@ -1,77 +1,34 @@
-
-export class UserInfo {
-
-  constructor(
-    userName, //container
-    userJob,
-    userPhoto,//container
-    inputName,
-    inputJob,
-    nameValueForm,//textCont
-    jobValueForm,//textCont
-    api
-  ) {
-    this.userName = userName;
-    this.userJob = userJob;
-    this.userPhoto = userPhoto;
-    this.inputName = inputName;
-    this.inputJob = inputJob;
-    this.api = api;
-    this.nameValueForm = nameValueForm;
-    this.jobValueForm = jobValueForm;
-    this.inputInfoApi = this.inputInfoApi.bind(this);
+export default class UserInfo {
+  constructor(username, userjob, avatar) {
+      this.userName = username;
+      this.userJob = userjob;
+      this.avatarElement = avatar;
   }
-  string(nameStr, jobStr) {
 
-
-    this.inputName.value = nameStr;
-    this.inputJob.value = jobStr;
+  setUserInfo(name, about, avatar) {
+      this.name = name;
+      this.about = about;
+      this.avatar = avatar;
   }
-  setUserInfoEdit() {
-    this.nameValueForm = this.inputName.value;
-    this.jobValueForm = this.inputJob.value;
+
+  getUserInfo() {
+      return {name: this.name, about: this.about};
+  }
+
+  updateUserInfo(name, about, avatar) {
+      this.setUserInfo(name, about, avatar);
+      this.userName.textContent = this.name;
+      this.userJob.textContent = this.about;
+      if (avatar) {
+          this.avatarElement.style.backgroundImage = `url('${avatar}')`;
+      }
 
   }
-  updateUserInfo (data) {
-    this.userName.textContent = data.name;
-    this.userJob.textContent = data.about;
-    this.userPhoto.setAttribute('style',
-      `background-image: url(${data.avatar})`);
+
+  create() {
+      this.setUserInfo(this.userName.textContent, this.userJob.textContent);
+      
+
   }
-  infoEditListener() {
-    document.querySelector('.user-info__edit-button').addEventListener('click', function () {
-      userInfo.string(document.querySelector(".user-info__name").textContent, document.querySelector(".user-info__job").textContent);
-    });
-
-  };
-  infoApi() {
-    this.api.getUserInfo()
-      .then((data) =>  {
-
-        return this.updateUserInfo(data);
-
-
-
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
-  }
-  inputInfoApi() {
-    this.api.setUserInfo(this.nameValueForm, this.jobValueForm)
-      .then(function(res)  {
-        popup.toggleEditPopup();
-        return this.updateUserInfo(res);
-
-
-
-
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  } /**/
 }
-
-
 
